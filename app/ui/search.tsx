e0cf4f49f091ec0f1c8168fed42bb72b70ca4c8f
lapinRagnar <lapinragnar@gmail.com>
2023-12-12 17:55:06 +0100

    useDebouncedCallback

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 import { useSearchParams, usePathname, useRouter  } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -10,9 +11,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  function handleSearch(term: string) {
+/*   function handleSearch(term: string) {
     console.log(`Searching... ${term}`)
-    
+
     const params = new URLSearchParams(searchParams)
     if (term) {
       params.set('query', term);
@@ -21,7 +22,19 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`)
 
-  }
+  } */
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+   
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 800)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
